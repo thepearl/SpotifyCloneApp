f2b02c7e516829dd94d425fb16726408b08e86c8
Ghazi Tozri <ghazi.tozri@wi-mobi.com>
2021-02-27 23:48:15 +0100

    init project

--- SpotifyCloneApp/AppDelegate.swift
+++ SpotifyCloneApp/AppDelegate.swift
@@ -10,27 +10,33 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window?.overrideUserInterfaceStyle = .light
+        
+        let rootNavC = UINavigationController(rootViewController: ViewController.instantiateFromStoryboard(MAIN_STORYBOARD))
+        rootNavC.isNavigationBarHidden = true
+        window?.rootViewController = rootNavC
+        window?.makeKeyAndVisible()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
+let MAIN_STORYBOARD = "Main"
+
+
+extension UIViewController
+{
+    class func instantiateFromStoryboard(_ name: String) -> Self
+    {
+        return instantiateFromStoryboardHelper(name)
+    }
+    
+    fileprivate class func instantiateFromStoryboardHelper<T>(_ name: String) -> T
+    {
+        let storyboard = UIStoryboard(name: name, bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: String(describing: self)) as! T
+        return controller
+    }}
